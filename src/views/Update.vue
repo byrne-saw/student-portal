107f9a069afe764e26fa8b7c0fa1327c37e67faf
“karimshakir” <karimshakir99@gmail.com>
2018-10-30 21:27:28 -0500

    stating education portion of update

--- src/views/Update.vue
+++ src/views/Update.vue
@@ -3,30 +3,19 @@
     <h1>this is the update page</h1>
 
   <form>
-    <div class="form-group">
-      <label for="exampleFormControlInput1">Email address</label>
-      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-    </div>
-    <div class="form-group">
-      <label for="exampleFormControlSelect1">Example select</label>
-      <select class="form-control" id="exampleFormControlSelect1">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="exampleFormControlSelect2">Example multiple select</label>
-      <select multiple class="form-control" id="exampleFormControlSelect2">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-      </select>
-    </div>
+ 
+ <h1>Login</h1>
+        <ul>
+          <li class="text-danger" v-for="error in errors">{{ error }}</li>
+        </ul>
+
+<div>  
+<input class="form-control form-control-sm" type="text" placeholder="Start Date">
+</div>
+
+
+        <input type="submit" class="btn btn-primary" value="Submit">
+
     <div class="form-group">
       <label for="exampleFormControlTextarea1">Example textarea</label>
       <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
@@ -40,13 +29,20 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
+  template: "#login-page",
   data: function() {
     return {
+      email: "",
+      password: "",
+      errors: []
     };
   },
-  created: function() {},
-  methods: {},
+  methods: {
+
+  },
   computed: {}
 };
 </script>
